a5a6b78f263f2ce275c29cdf95dfc7847cf96c7e
Wahyu Maulana <mwahyu7403@gmail.com>
2024-03-07 09:57:23 +0700

    operator_aritmatika

---
+++ dart_project/bin/01_operator_aritmatika.dart
@@ -0,0 +1,15 @@
+void main(){
+  int a = 5;
+  int b = 10;
+
+  var penjumlahan = a+b;
+  var pengurangan = a-b;
+  var perkalian = a*b;
+  var pembagian = a/b;
+  var hasilBagi = a%b;
+  print("Penjumlahan (+) $a dan $b menghasilkan: $penjumlahan");
+  print("Pengurangan (-) $a dan $b menghasilkan: $penjumlahan");
+  print("Perkalian (*) $a dan $b menghasilkan  : $penjumlahan");
+  print("Pembagian (/) $a dan $b menghasilkan  : $penjumlahan");
+  print("Hasil Bagi (%) $a dan $b menghasilkan : $penjumlahan");
+}
